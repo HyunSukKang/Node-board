6ea06435e7aa72a2019455ef8adf9ae7108d158d
HyunSukKang <hsuk6032@gmail.com>
2021-12-06 18:32:40 +0900

    title, body search

--- routes/posts.js
+++ routes/posts.js
@@ -10,10 +10,12 @@ router.get('/', async function(req, res) {
   page = !isNaN(page)?page:1;
   limit = !isNaN(limit)?limit:10;
 
+  var searchQuery = createSearchQuery(req.query);
+
   var skip = (page-1) * limit;
-  var count = await Post.countDocuments({});
+  var count = await Post.countDocuments(searchQuery);
   var maxPage = Math.ceil(count/limit);
-  var posts = await Post.find({})
+  var posts = await Post.find(searchQuery)
     .populate('author')
     .sort('-createdAt')
     .skip(skip)
@@ -25,6 +27,8 @@ router.get('/', async function(req, res) {
     currentPage:page,
     maxPage:maxPage,
     limit:limit,
+    searchType:req.query.searchType,
+    searchText:req.query.searchText,
   });
 });
 
@@ -44,7 +48,7 @@ router.post('/', util.isLoggedin, function(req, res) {
       req.flash('errors', util.parseError(err));
       return res.redirect('/posts/new' + res.locals.getPostQueryString());
     }
-    res.redirect('/posts' + res.locals.getPostQueryString(false, {page:1}));
+    res.redirect('/posts' + res.locals.getPostQueryString(false, {page:1, searchText:''}));
   });
 });
 
@@ -106,4 +110,21 @@ function checkPermission(req, res, next) {
 
     next();
   });
+}
+
+function createSearchQuery(queries) {
+  var searchQuery = {};
+  if(queries.searchType && queries.searchText && queries.searchText.length >= 3) {
+    var searchTypes = queries.searchType.toLowerCase().split(',');
+    var postQueries = [];
+    if(searchTypes.indexOf('title') >= 0) {
+      postQueries.push({ title: { $regex: new RegExp(queries.searchText, 'i')}});
+    }
+    if(searchTypes.indexOf('body') >= 0) {
+      postQueries.push({ body: { $regex: new RegExp(queries.searchText, 'i')}});
+    }
+    if(postQueries.length > 0) searchQuery = {$or:postQueries};
+  }
+
+  return searchQuery;
 }
